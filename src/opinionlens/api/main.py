4dd61517c6f8cadf93836a0af41e8d8b168bded8
Abdelaziz W. Farahat <abdelaziz.w.f@gmail.com>
2025-12-22 11:17:43 +0200

    Add license and API info

--- src/opinionlens/api/main.py
+++ src/opinionlens/api/main.py
@@ -6,7 +6,33 @@ from opinionlens.api.exceptions import ModelNotAvailableError, OperationalError
 from opinionlens.api.managers import model_manager
 from opinionlens.api.routers import private
 
-app = FastAPI()
+description = """
+A production-ready sentiment analysis pipeline leveraging local ML training, DVC data versioning, MLflow model registry, Dockerized services, and Prometheus/Grafana monitoring.
+
+## Planned Features
+
+- Monitoring stack with Prometheus and Grafana
+- Reverse proxy configuration with Traefik
+- Training and deploying deep models
+- Model interpretability
+- More raw data and using sampling techniques for training data
+- Extensive testing
+"""
+
+app = FastAPI(
+    title="OpinionLens",
+    description=description,
+    summary="AI-powered sentiment analysis with reproducible models and scalable deployment.",
+    version="0.0.2",
+    contact={
+        "name": "Abdelaziz W. Farahat",
+        "email": "abdelaziz.w.f@gmail.com",
+    },
+    license_info={
+        "name": "Apache-2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
 
 app.include_router(private.router)
 
